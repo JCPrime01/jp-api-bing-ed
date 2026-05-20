13790e0a579946caa918dc3568d9ae10b3fb763c
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-19 21:52:14 -0300

    chore: update shareCode to ATN7IPE84XZ

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/hncv0ue6?shareCode=5033880EJTK&afp=tapib' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/hncv0ue6?shareCode=ATN7IPE84XZ&afp=tapib' });
     res.end();
   }
