3cdd2b56519cbbf337bcfa2d72ac6f6be9f5aea8
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-25 20:38:10 -0300

    chore: update shareCode to DLCWVBULSCJ

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/hncv0ue6?shareCode=Y1WT1QCRWAD&afp=tapib' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/hncv0ue6?shareCode=DLCWVBULSCJ&afp=tapib' });
     res.end();
   }
